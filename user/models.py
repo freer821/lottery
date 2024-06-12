4d3350b83c587e61c22ef7274524f5c777d64397
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2024-06-12 17:28:28 +0200

    update

--- user/models.py
+++ user/models.py
@@ -4,28 +4,13 @@ from django.db.models import JSONField
 
 # Create your models here.
 class Customer(models.Model):
-    type = models.CharField(max_length=80, default="") # new customer or registered
-    customer_no = models.CharField(max_length=80, unique=True, blank=True, null=True)
     company = models.CharField(max_length=80, default="")
     sex = models.CharField(max_length=20, default="")
     first_name = models.CharField(max_length=100, default="")
     last_name = models.CharField(max_length=100, default="")
-    company_extra = models.CharField(max_length=200, default="")
-    addr = models.CharField(max_length=250, default="")
-    addr_extra = models.CharField(max_length=250, default="")
-    city = models.CharField(max_length=100, default="")
-    postcode = models.CharField(max_length=20, default="")
     country = models.CharField(max_length=30, default="")
     tel = models.CharField(max_length=50, default="")
-    fax = models.CharField(max_length=50, default="")
     email = models.CharField(max_length=100, default="")
-    bill_email = models.CharField(max_length=100, default="")
-    website = models.CharField(max_length=150, default="")
-    tax_id = models.CharField(max_length=100, default="")
-    ust_id = models.CharField(max_length=100, default="")
-    bank = models.CharField(max_length=100, default="")
-    iban = models.CharField(max_length=30, default="")
-    bic = models.CharField(max_length=10, default="")
     contact = JSONField(default=dict)
     is_won = models.BooleanField(default=False)
     createdtime = models.DateTimeField(auto_now_add=True)
--- user/serializers.py
+++ user/serializers.py
@@ -14,42 +14,19 @@ class CustomerSerializer(serializers.ModelSerializer):
     createdtime = serializers.DateTimeField(format="%Y-%m-%d %H:%M", allow_null=True, required=False)
     updatedtime = serializers.DateTimeField(format="%Y-%m-%d %H:%M", allow_null=True, required=False)
     contact = serializers.JSONField(required=False)
-    customer_no = serializers.CharField(required=False, allow_blank=True)
-    company_extra = serializers.CharField(required=False, allow_blank=True)
-    addr_extra = serializers.CharField(required=False, allow_blank=True)
-    fax = serializers.CharField(required=False, allow_blank=True)
-    website = serializers.CharField(required=False, allow_blank=True)
-    bank = serializers.CharField(required=False, allow_blank=True)
-    iban = serializers.CharField(required=False, allow_blank=True)
-    bic = serializers.CharField(required=False, allow_blank=True)
     is_won = serializers.BooleanField(required=False)
 
     class Meta:
         model = Customer
         fields = '__all__'
         extra_kwargs = {
-            'type': {'required': True},
-            'customer_no': {'required': False},
             'company': {'required': True},
             'sex': {'required': True},
             'first_name': {'required': True},
             'last_name': {'required': True},
-            'company_extra': {'required': False},
-            'addr': {'required': True},
-            'addr_extra': {'required': False},
-            'postcode': {'required': True},
-            'city': {'required': True},
             'country': {'required': True},
             'tel': {'required': True},
-            'fax': {'required': False},
             'email': {'required': True},
-            'bill_email': {'required': True},
-            'website': {'required': False},
-            'tax_id': {'required': True},
-            'ust_id': {'required': True},
-            'bank': {'required': False},
-            'iban': {'required': False},
-            'bic': {'required': False},
             'is_won': {'required': False}
         }
 
